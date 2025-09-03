f07c91fccb5a5a1614812b7e138778637e02cebf
Atul kumar jha <jhaak301@gmail.com>
2025-09-03 17:15:02 +0530

    Working on file uploading system

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,27 +1,39 @@
 export default function Navbar() {
-    return (
-        <nav 
+  return (
+    <div style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "60px",
+        backgroundColor: "#001A00",
+        color: "white",
+        padding: "1rem 1.5 rem",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        boxShadow:
+          "0 4px 6px -1px rgba(0,0,0,0), 0 2px 4px -1px rgba(0,0,0,0.0)",
+        zIndex: 50,
+        border: 'dotted 2px darkgreen ',
+        margin: '10px auto',
+      }}>
+    <nav
+      
+    >
+      <div
         style={{
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "60px",
-            backgroundColor: "#1f2937",
-            color: "white",
-            padding: "1rem 1.5 rem",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-            boxShadow: "0 4px 6px -1px rgba(0,0,0,0.1), 0 2px 4px -1px rgba(0,0,0,0.06)",
-            zIndex: 50,
-        }}>
-            <div style={{
-                marginLeft: "1rem"
-            }}><h1 style={{
-                color: "green",
-            }}
-            >SealData</h1></div>
-        </nav>
-    )
+          marginLeft: "1.5rem",
+        }}
+      >
+        <h1>
+          <span style={{
+            color:"rgba(144, 238, 144, 1)"
+          }}>Seal</span>
+          <span>Data</span>
+        </h1>
+      </div>
+    </nav>
+    </div>
+  );
 }
---
+++ src/app/components/select.tsx
@@ -0,0 +1,18 @@
+import { Upload } from 'lucide-react';
+
+export default function Select() {
+    return (
+        <div style={{
+        display: 'flex',
+        justifyContent: "center",
+        alignItems: "center",
+        height: '50vh',
+        width: '40vw',
+        border: 'dotted 2px darkgreen',
+        margin: "100px 50px 100px 50px",
+        backgroundColor: 'transparent',
+        }}>
+        
+        </div>
+    )
+}
