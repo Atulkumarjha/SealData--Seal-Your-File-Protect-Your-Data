2fb5e68e9e20031d80cf7bbbde3d09b674fa0c91
Atul kumar jha <jhaak301@gmail.com>
2025-09-04 12:16:10 +0530

    Done with components

--- src/app/components/select.tsx
+++ src/app/components/select.tsx
@@ -1,18 +1,97 @@
-import { Upload } from 'lucide-react';
+"use client";
 
+import { useState } from "react";
+import { Upload } from "lucide-react";
 export default function Select() {
-    return (
-        <div style={{
-        display: 'flex',
+  const [hover, setHover] = useState(false);
+  return (
+    <div
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      style={{
         justifyContent: "center",
         alignItems: "center",
-        height: '50vh',
-        width: '40vw',
-        border: 'dotted 2px darkgreen',
-        margin: "100px 50px 100px 50px",
-        backgroundColor: 'transparent',
-        }}>
-        
+        height: "35vh",
+        width: "40vw",
+        border: "dotted 2px darkgreen",
+        margin: "100px 50px 100px 25px",
+        backgroundColor: "black",
+        borderRadius: "10px",
+        paddingBottom: "350px",
+      }}
+    >
+      {" "}
+      <div>
+        <p
+          style={{
+            color: "white",
+            marginLeft: "300px",
+            marginTop: "20px",
+            fontWeight: "bold",
+            fontSize: "25px",
+          }}
+        >
+          Upload file
+        </p>
+        <p
+          style={{
+            color: "#989898",
+            marginLeft: "230px",
+            marginTop: "20px",
+          }}
+        >
+          Drag and Drop here or Click to upload files
+        </p>
+      </div>
+      <div
+        style={{
+          height: "150px",
+          width: "600px",
+          backgroundColor: "transparent",
+          marginTop: "150px",
+          position: "relative", 
+        }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            top: "0",
+            left: "310px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100px",
+            width: "100px",
+            backgroundColor: hover ? "#0d0d0d" : "transparent", 
+            borderRadius: "10px",
+            transition: "background-color 0.3s ease",
+            border: "dotted 2px darkgreen",
+          }}
+        />
+
+        <div
+          style={{
+            position: "absolute",
+            top: "0",
+            left: "310px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100px",
+            width: "100px",
+            backgroundColor: "#171717",
+            borderRadius: "10px",
+            transform: hover ? "translate(30px, -30px)" : "translate(0, 0)",
+            boxShadow: hover
+              ? "0px 15px 40px rgba(0,0,0,0.6)"
+              : "0px 5px 20px rgba(0,0,0,0.2)",
+            transition: "transform 0.3s ease, box-shadow 0.3s ease",
+            zIndex: 2, 
+          }}
+        >
+          <Upload />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
